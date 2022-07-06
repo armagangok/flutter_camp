ce730fcc39edc7bcff2cfc620f3368cfb6dd02d3
Armağan Gök <1armagangok@gmail.com>
2022-07-06 14:53:32 +0300

    initial commit

--- socket_server/bin/socket_server.dart
+++ socket_server/bin/socket_server.dart
@@ -1,5 +1,47 @@
-import 'package:socket_server/socket_server.dart' as socket_server;
+import 'dart:io';
+import 'dart:typed_data';
 
-void main(List<String> arguments) {
-  print('Hello world: ${socket_server.calculate()}!');
+
+void main() async {
+  // bind the socket server to an address and port
+  final server = await ServerSocket.bind(InternetAddress.anyIPv4, 4567);
+
+  // listen for clent connections to the server
+  server.listen((client) {
+    handleConnection(client);
+  });
+}
+
+void handleConnection(Socket client) {
+  print('Connection from'
+      ' ${client.remoteAddress.address}:${client.remotePort}');
+
+  // listen for events from the client
+  client.listen(
+    // handle data from the client
+    (Uint8List data) async {
+      await Future.delayed(Duration(seconds: 1));
+      final message = String.fromCharCodes(data);
+      if (message == 'Knock, knock.') {
+        client.write('Who is there?');
+      } else if (message.length < 10) {
+        client.write('$message who?');
+      } else {
+        client.write('Very funny.');
+        client.close();
+      }
+    },
+
+    // handle errors
+    onError: (error) {
+      print(error);
+      client.close();
+    },
+
+    // handle the client closing the connection
+    onDone: () {
+      print('Client left');
+      client.close();
+    },
+  );
 }
